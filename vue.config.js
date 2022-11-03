be0cf06577f4ebadad98c7b85bc12103484e836f
abdulla <abdulla.arish@protonmail.com>
2022-11-03 22:43:37 +0100

    deployment

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/task-tracker/'
-    : '/'
-}
+
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+
+})
